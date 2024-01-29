2eb6fcfc29cc3550d03443ddefeec114219c5ee3
Mateus Sousa Pimenta <137216621+mspimenta@users.noreply.github.com>
2024-01-29 19:33:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById('color1').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'yellow';
+  });
+  
+  document.getElementById('color2').addEventListener('mouseover', function() {
+    document.body.style.backgroundColor = 'red';
+  });
+  
+  document.getElementById('color3').addEventListener('dblclick', function() {
+    document.body.style.backgroundColor = 'blue';
+  });
